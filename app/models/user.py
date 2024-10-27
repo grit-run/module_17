0f6b3f2c89359b3f23645e1ee20c04e93c56f07e
elinsky <dmitry.ilinsky@gmail.com>
2024-10-27 15:30:12 +0300

    sceleton updated by backend and models

---
+++ app/models/user.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import CreateTable
+
+from app.backend.db import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    __table_args__ = {"extend_existing": True}
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(String)
+    slug = Column(String, unique=True, index=True)
+    tasks = relationship("Task", back_populates="user")
+
+
+print(CreateTable(User.__table__))
